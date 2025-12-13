206d1cb242d416fc0757aedb891798ed425ff983
dhanush <dhanushpanga1030@gmail.com>
2025-12-14 01:55:47 +0530

    Implementation details

--- Frontend/client/src/components/examples/TeacherDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import TeacherDashboard from '../TeacherDashboard';
-
-export default function TeacherDashboardExample() {
-  return (
-    <TeacherDashboard 
-      onDownloadReport={(classGrade) => console.log('Download report for class:', classGrade)}
-    />
-  );
-}
